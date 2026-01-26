621f1ae3199ee45f8b1367fa0660400c84509b1f
PelletiBhargav <pelletivenkatabhargav03@gmail.com>
2026-01-26 16:42:14 +0530

    some changes

--- networksecurity/compontents/model_trainer.py
+++ networksecurity/compontents/model_trainer.py
@@ -24,6 +24,8 @@ from sklearn.ensemble import (
 )
 import mlflow
 
+import dagshub
+dagshub.init(repo_owner='PelletiBhargav', repo_name='Network_Security', mlflow=True)
 
 
 
@@ -73,19 +75,19 @@ class ModelTrainer:
                 # 'criterion':['gini', 'entropy', 'log_loss'],
                 
                 # 'max_features':['sqrt','log2',None],
-                'n_estimators': [8,16,32,64,128,256]
+                'n_estimators': [8,16,32,128,256]
             },
             "Gradient Boosting":{
                 # 'loss':['log_loss', 'exponential'],
                 'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                'subsample':[0.6,0.7,0.75,0.85,0.9],
                 # 'criterion':['squared_error', 'friedman_mse'],
                 # 'max_features':['auto','sqrt','log2'],
                 'n_estimators': [8,16,32,64,128,256]
             },
             "Logistic Regression":{},
             "AdaBoost":{
-                'learning_rate':[.1,.01,0.5,.001],
+                'learning_rate':[.1,.01,.001],
                 'n_estimators': [8,16,32,64,128,256]
             }
             
